1b8db51c36564ad06e06f613f2afd886ef3345f4
Ant Perry <ant290@live.co.uk>
2026-04-03 00:51:30 +0100

    speed limiting

--- g-racer/vehicles/sports_car/sports_car_from_tut.gd
+++ g-racer/vehicles/sports_car/sports_car_from_tut.gd
@@ -4,6 +4,11 @@ extends VehicleBody3D
 @export var ENGINE_POWER := 300
 @export var BRAKE_POWER := 1.5
 
+## Maximum speed in kilometres per hour
+@export var MAX_SPEED := 220
+## Maximum speed in kilometres per hour
+@export var MAX_REVERSE_SPEED := 80
+
 @onready var camera_pivot: Node3D = $CameraPivot
 @onready var camera_3d: Camera3D = $CameraPivot/Camera3D
 @onready var reverse_camera: Camera3D = $CameraPivot/ReverseCamera
@@ -37,16 +42,26 @@ func _input(event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var speed = linear_velocity.length()
+	var speed_in_kph = speed * 3.6
 	
 	steering = move_toward(steering, Input.get_axis("driving_right", "driving_left") * MAX_STEER, delta * 2.5)
 	var forward_input = Input.get_axis("driving_reverse", "driving_accelerate")
+	var velocity_dot = linear_velocity.dot(transform.basis.z)
+	
 	if forward_input < 0:
-		if speed < 5.0 and not is_zero_approx(speed):
-			engine_force = -clampf(ENGINE_POWER * BRAKE_POWER * 5.0 / speed, 0.0, 150)
+		## Limit to max reverse speed
+		if speed_in_kph >= MAX_REVERSE_SPEED and velocity_dot < 0:
+			engine_force = 0
 		else:
-			engine_force = -ENGINE_POWER * BRAKE_POWER
+			if speed < 5.0 and not is_zero_approx(speed):
+				engine_force = -clampf(ENGINE_POWER * BRAKE_POWER * 5.0 / speed, 0.0, 150)
+			else:
+				engine_force = -ENGINE_POWER * BRAKE_POWER
 	else:
-		engine_force = forward_input * ENGINE_POWER
+		if speed_in_kph >= MAX_SPEED and velocity_dot > 0:
+			engine_force = 0
+		else:
+			engine_force = forward_input * ENGINE_POWER
 
 	#camera controls
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20)
